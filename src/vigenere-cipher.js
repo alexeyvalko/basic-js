672d882ad1a4f79432e829123419d1ce2a7e6d69
alexeyvalko <valko.alexey@gmail.com>
2021-03-29 20:05:54 +0300

    feat: cipher task is done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,65 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    if (isDirect === false) {
+      this.isDirect = false;
+    } else {
+      this.isDirect = true;
+    }
+  }
+
+  encrypt(message, key) {
+    let msg = message.toUpperCase();
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+
+    while (key.length <= message.length) {
+      key += key;
+    }
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let m = message.toUpperCase().charCodeAt(i);
+      let k = key.toUpperCase().charCodeAt(j);
+
+      if (m >= 65 && m <= 90) {
+        result += String.fromCharCode(((m + k - 130) % 26) + 65);
+        j++;
+      } else {
+        result += message[i].toUpperCase();
+      }
+    }
+
+    return this.isDirect ? result : result.split("").reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error();
+    }
+    while (key.length <= encryptedMessage.length) {
+      key += key;
+    }
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      let m = encryptedMessage.toUpperCase().charCodeAt(i);
+      let k = key.toUpperCase().charCodeAt(j);
+
+      if (m >= 65 && m <= 90) {
+        result += String.fromCharCode(((m - k + 26) % 26) + 65);
+        j++;
+      } else {
+        result += encryptedMessage[i].toUpperCase();
+      }
+    }
+
+    return this.isDirect ? result : result.split("").reverse().join("");
   }
 }
 
